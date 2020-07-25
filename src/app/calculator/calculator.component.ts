958e3a9e34c93157ca722b5eb6e52a49147fac8d
Wellington Cunha <wdcunha@gmail.com>
2020-07-25 16:21:48 +0100

    calculator finished

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,9 +7,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
+  currentNumber = '0';
+  firstOperand = null;
+  operator = null;
+  waitForSecondNumber = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  /**
+   * used to get the current number
+   * @param v is the value received from html view
+   */
+  public getNumber(v: string): void {
+    console.log(v);
+    if (this.waitForSecondNumber) {
+      this.currentNumber = v;
+      this.waitForSecondNumber = false;
+    } else {
+      this.currentNumber === '0' ? this.currentNumber = v : this.currentNumber += v;
+    }
+  }
+
+  /**
+   * appends the decimal point to the current number
+   */
+  getDecimal(): void {
+    if (!this.currentNumber.includes('.')) {
+      this.currentNumber += '.';
+    }
+  }
+
+  /**
+   * performs the calculation depending on the operator type
+   * @param op contains the operation signal typed by the user
+   * @param secondOp contains the second number (or third...) typed by the user
+   */
+  private  doCalculation(op, secondOp): number {
+    switch (op) {
+      case '+':
+        return this.firstOperand += secondOp;
+      case '-':
+        return this.firstOperand -= secondOp;
+      case '*':
+        return this.firstOperand *= secondOp;
+      case '/':
+        return this.firstOperand /= secondOp;
+      case '=':
+        return secondOp;
+    }
+  }
+
+  /**
+   * get the performed operation
+   * @param op brings the operation signal typed by the user
+   */
+  public getOperation(op: string): void {
+    console.log(op);
+    if (this.firstOperand === null) {
+      this.firstOperand = Number(this.currentNumber);
+    } else  if (this.operator) {
+      const result = this.doCalculation(this.operator, Number(this.currentNumber));
+      this.currentNumber = String(result);
+      this.firstOperand = result;
+    }
+    this.operator = op;
+    this.waitForSecondNumber = true;
+
+    console.log(this.firstOperand);
+  }
+
+  /**
+   * clear the result area and reset the calculations
+   */
+  public clear(): void {
+    this.currentNumber = '0';
+    this.firstOperand = null;
+    this.operator = null;
+    this.waitForSecondNumber = false;
+  }
 }
